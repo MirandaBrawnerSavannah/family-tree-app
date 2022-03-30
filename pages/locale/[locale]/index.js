0e0c337037e0d34a580872b9b4e0275ecdc03753
Miranda Brawner <Miranda.Brawner@aexp.com>
2022-03-30 15:33:47 -0400

    feat(page-heading): added page heading saying Create a Family Tree

--- pages/locale/[locale]/index.js
+++ pages/locale/[locale]/index.js
@@ -1,28 +1,20 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import LanguageList from '../../../components/LanguageList'
+import Localizer from '../../../utils/Localizer';
 
 const MainMenu = () => {
   const router = useRouter();
   const { locale } = router.query;
+  const intl = new Localizer(locale);
 
   return (
-    <>
+    <div>
       <LanguageList />
-      <h1>Locale: {locale}</h1>
+      <h1>{intl.formatMessage({ id: 'createFamilyTree' })}</h1>
       <ul>
-        <li>
-          <Link href="/locale/[locale]/[comment]" as={`/locale/${locale}/first-comment`}>
-            <a>First comment</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/locale/[locale]/[comment]" as={`/locale/${locale}/second-comment`}>
-            <a>Second comment</a>
-          </Link>
-        </li>
       </ul>
-    </>
+    </div>
   );
 };
 
