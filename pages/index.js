d9a3745901029aac66ed410337df6378ca14cf52
Miranda Brawner <Miranda.Brawner@aexp.com>
2022-04-01 09:18:29 -0400

    feat(redirect-from-main-page): used router.push to automatically load the english version of the tree from the main page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import LanguageList from '../components/LanguageList'
 
 const Home = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/locale/en');
+  });
   return (
     <div>
       <LanguageList />
