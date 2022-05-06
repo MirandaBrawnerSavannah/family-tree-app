3d7cd87a1c434352645aa4a3957b09808ed3a9e8
Miranda Brawner <Miranda.Brawner@aexp.com>
2022-05-06 12:29:48 -0400

    feat(update-query-param-with-hashtags): expanded the updateQueryParam function
    so that it can handle html element id names with hashtags

--- utils/updateQueryParam.js
+++ utils/updateQueryParam.js
@@ -1,9 +1,18 @@
 const updateQueryParam = ({ path, paramName, paramValue }) => {
+  const hashTagIndex = path.indexOf('#');
+  if (hashTagIndex !== -1) {
+    const beforeHashTag = path.slice(0, hashTagIndex); // does not include #
+    const hashTag = path.slice(hashTagIndex);
+    const newPageUrl = updateQueryParam ({ 
+      path: beforeHashTag, paramName, paramValue,
+    });
+    return `${newPageUrl}${hashTag}`;
+  }
   const questionMarkIndex = path.indexOf('?');
   if (questionMarkIndex === -1) {
     return `${path}?${paramName}=${paramValue}`
   }
-  const pathWithoutParams = path.slice(0, questionMarkIndex + 1);
+  const pathWithoutParams = path.slice(0, questionMarkIndex + 1); // includes ?
   const paramList = path.slice(questionMarkIndex + 1).split('&')
   let foundMatchingParam = false;
   const newParamList = paramList.map((nextNameAndValue) => {
