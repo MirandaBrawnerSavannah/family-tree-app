4b7fe9b3340a9b95f0278c9483e48b75d4fcc58b
Miranda Brawner <Miranda.Brawner@aexp.com>
2022-06-01 13:52:14 -0400

    feat(marriage-dates): tree takes into account wedding dates

--- utils/filterByDate.js
+++ utils/filterByDate.js
@@ -18,15 +18,36 @@ const wasBornBefore = ({ person, date }) => {
   return isBefore({ firstDate: person.born, secondDate: date });
 }
 
+const updateMarriageEndDate = ({ marriage, date }) => {
+  if (isBefore({ firstDate: date, secondDate: marriage.endDate })) {
+    return { ...marriage, endDate: undefined };
+  }
+  return marriage;
+}
+
+const filterMarriages = ({ person, date }) => {
+  const marriages = person.marriedTo;
+  if (marriages) {
+    const filteredMarriages = marriages
+      .map((marriage) => updateMarriageEndDate({ marriage, date }))
+      .filter((marriage) => isBefore(
+        { firstDate: marriage.startDate, secondDate: date }
+      ));
+    return { ...person, marriedTo: filteredMarriages };
+  }
+  return person; 
+}
+
 const filterByDate = ({ listOfPeople, date }) => {
   const peopleBornBefore = listOfPeople.filter((person) => {
     return wasBornBefore({ person, date });
   });
   const listCopy = peopleBornBefore.map((person) => {
+    const personAtTime = filterMarriages({ person, date });
     if (isBefore({ firstDate: person.died, secondDate: date })) {
-      return { ...person };
+      return { ...personAtTime };
     } else {
-      return { ...person, died: undefined };
+      return { ...personAtTime, died: undefined };
     }
   })
   return listCopy;
